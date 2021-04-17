2aecbd6a0fca76408984c54df0283304d1d8ab89
adriangvaldes <adriangvaldes@gmail.com>
2021-04-16 21:36:48 -0300

    init

---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    white: {
+      "100" : "#F5F8FA"
+    },
+    gray: {
+      "100" : "#999999",
+      "400" : "#47585B",
+    }
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: '#F5F8FA',
+        color: '#47585B'
+      }
+    }
+  }
+})
